3e4af2f21a4df9d788501581209a5c66483e5a52
schukwuemeka <uchechukwuemekasamue@gmail.com>
2022-03-16 22:25:26 -0400

    my first commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,14 @@
-#include <stdio.h>
+#include "main.h"
+int main (void)
+{
+char *sh = "Alx";
 
-void main() {
-int c;
-
-c = getchar();
-while (c != EOF) {
-purchar(c);
-purchar('\n');
-c = getchar();
+while (*sh)
+{
+ _putchar(*sh);
+sh++;
 }
+ _putchar('\n');
+
+return (0);
 }
