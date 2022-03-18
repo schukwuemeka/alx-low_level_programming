f473f9d631af2dea4fe0ba9dbb268d47aed61d0a
schukwuemeka <uchechukwuemekasamue@gmail.com>
2022-03-17 22:08:12 -0400

    my commit message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
 * Return: 0 if success.
 */
 int main(void)
-{\
-puts("programming is like building a multilingual puzzle \n");
-retgurn (0);
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
